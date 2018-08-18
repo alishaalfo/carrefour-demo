0ba856e683e98d670010ec6a14fba2d004561d26
Alisha <alishaalfo@gmail.com>
2018-08-18 16:35:47 +0400

    login page

---
+++ js/app.js
@@ -0,0 +1,42 @@
+$('#loginForm').validate({
+    rules: {
+        username: {
+            required: true,
+        },
+        password: {
+            required: true,
+        }, 
+        },
+        messages: {
+        username: {
+            required: "Required field",
+        },
+        password: {
+            required: "Required field",
+        }
+    },
+    submitHandler: function(form, e) {
+    const username =$("#username").val();
+    const password =$("#password").val(); 
+        $.ajax({
+          url: "users.php",
+          type: "POST",
+          data: {
+            username:username,
+            password:password
+          },
+        success: function() {
+            swal({
+                title: "Welcome to Carrefour!",
+                text: "You have logged in successfully",
+                icon: "success",
+                button: "Let's explore",
+            });
+        },
+        error : function() {
+          alert('Error')
+        }
+        
+      });
+    }
+});
---
+++ users.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "<pre>";
+header("HTTP/1.1 200 OK");
+
+print_r($_POST);
+
+?>
